26309fd32b5e2612993a8d2cadafe04b1953bf3d
OlgaZa <[omzakharova@gmail.com]>
2024-01-27 19:20:23 +0200

    CompaniesDAO interface is fully implemented by CompaniesDBDAO

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import dao.entities.Company;
 import dao.operations.CompaniesDBDAO;
 
+import java.util.Objects;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -20,6 +22,31 @@ public class Main {
             System.out.println("company exists");
         } else {
             System.out.println("company does not exists");
+        }
+
+        var company1 = dao.getOneCompany(company.Id);
+        if (company1 == null) {
+            throw new Exception("pizdetz one comany");
+        }
+
+
+
+        company1.name = "new name";
+        company1.email = "new email";
+        company1.password = "new password";
+
+        dao.updateCompany(company1);
+        var updatedCompany = dao.getOneCompany(company1.Id);
+
+        if (!Objects.equals(company1.name, updatedCompany.name) ||
+                !Objects.equals(company1.email, updatedCompany.email) ||
+                !Objects.equals(company1.password, updatedCompany.password)) {
+            throw new Exception("update failed");
+        }
+
+        var allCompanies = dao.getAllCompanies();
+        if (allCompanies.isEmpty()) {
+            throw new Exception("pizdets - no companies");
         }
     }
 }
--- src/dao/operations/CompaniesDBDAO.java
+++ src/dao/operations/CompaniesDBDAO.java
@@ -54,8 +54,7 @@ public class CompaniesDBDAO implements CompaniesDAO {
                     // the result set shall contain the generated ID as in its first column
                     if (generatedKeys.next()) {
                         company.Id = generatedKeys.getInt(1);
-                    }
-                    else {
+                    } else {
                         throw new SQLException("creating user failed, no ID obtained.");
                     }
                 }
@@ -68,20 +67,105 @@ public class CompaniesDBDAO implements CompaniesDAO {
     @Override
     public void updateCompany(Company company) {
 
+        try {
+            var conn = ConnectionPool.getInstance().getConnection();
+            var sql = "update companies set name=?, email=?, password=? where id=?";
+
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+                // set actual values of parameters
+                stmt.setString(1, company.name);
+                stmt.setString(2, company.email);
+                stmt.setString(3, company.password);
+                stmt.setInt(4, company.Id);
+
+                // and execute the insert
+                var updatedRows = stmt.executeUpdate();
+
+                if (updatedRows == 0) {
+                    throw new SQLException("updating company has failed, no rows affected.");
+                }
+            }
+        } catch (InterruptedException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public void deleteCompany(Company company) {
+        try {
+            var conn = ConnectionPool.getInstance().getConnection();
+            var sql = "delete from companies where id=?";
+
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+                // set actual values of parameters
+                stmt.setInt(1, company.Id);
+
+                // and execute the insert
+                var deletedRows = stmt.executeUpdate();
+
+                if (deletedRows == 0) {
+                    throw new SQLException("deletion of company has failed, no rows affected.");
+                }
+            }
+        } catch (InterruptedException | SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public Company getOneCompany(int companyId) {
-        return null;
+        try {
+            var conn = ConnectionPool.getInstance().getConnection();
+            var sql = "select id, name, email, password from companies where id = ?";
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setInt(1, companyId);
+                try (ResultSet rs = stmt.executeQuery()) {
+                    if (rs == null || !rs.next()) {
+                        return null;
+                    }
+                    var company = new Company(
+                            rs.getString(2),
+                            rs.getString(3),
+                            rs.getString(4));
+                    company.Id = rs.getInt(1);
+                    return company;
+                }
+            }
+        } catch (InterruptedException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public ArrayList<Company> getAllCompanies() {
-        return null;
+        try {
+            var conn = ConnectionPool.getInstance().getConnection();
+            var sql = "select id, name, email, password from companies";
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+                try (ResultSet rs = stmt.executeQuery()) {
+                    if (rs == null) {
+                        return null;
+                    }
+                    var result = new ArrayList<Company>();
+
+                    while (rs.next()) {
+                        var company = new Company(
+                                rs.getString(2),
+                                rs.getString(3),
+                                rs.getString(4));
+                        company.Id = rs.getInt(1);
+                        result.add(company);
+                    }
+                    return result;
+                }
+            }
+        } catch (InterruptedException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
