7388bc3113074edaae24335304fc5d95cecc9f67
Denis-laptop <denisshatik@gmail.com>
2024-03-07 21:05:46 +0200

    Started Job thread

--- src/cls/job/thread/CouponExpirationDailyJob.java
+++ src/cls/job/thread/CouponExpirationDailyJob.java
@@ -1,8 +1,35 @@
 package cls.job.thread;
 
+import cls.coupon.dao.impl.CouponsDaoImpl;
+
 public class CouponExpirationDailyJob implements Runnable {
+
+    private CouponsDaoImpl couponsDao;
+    private boolean quit;
+    private static CouponExpirationDailyJob instance = null;
+
+    private CouponExpirationDailyJob() {
+
+    }
+
+    //singleton instance
+    public static CouponExpirationDailyJob getInstance() {
+        if (instance == null) {
+            synchronized (CouponExpirationDailyJob.class) {
+                if (instance == null) {
+                    instance = new CouponExpirationDailyJob();
+                }
+            }
+        }
+        return instance;
+    }
+
     @Override
     public void run() {
+
+    }
+
+    public void stop() {
 
     }
 }
